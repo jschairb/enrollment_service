b95a7c697eac9ce6698baca23ffb5d34d58ea9da
Josh Schairbaum <joshua.schairbaum@gmail.com>
2012-02-04 17:57:12 -0600

    configuring for a possible heroku install

--- db/migrate/20120204202555_create_enrollments.rb
+++ db/migrate/20120204202555_create_enrollments.rb
@@ -1,126 +1,126 @@
 class CreateEnrollments < ActiveRecord::Migration
   def change
     create_table :enrollments do |t|
-      t.text :student_grade
-      t.text :student_first_name
-      t.text :student_last_name
-      t.text :student_middle_initial
-      t.text :student_nickname
-      t.text :student_rank
-      t.text :student_total
-      t.text :student_ssn
-      t.text :student_gender
-      t.text :student_street
-      t.text :student_city
-      t.text :student_state
-      t.text :student_zip
-      t.text :student_phone
-      t.text :student_listed_in_directory
-      t.text :student_dob
-      t.text :student_dob_city
-      t.text :student_dob_state
-      t.text :student_high_school
-      t.text :student_district
-      t.text :student_race
-      t.text :student_religion
-      t.text :student_church
-      t.text :student_last_school
-      t.text :student_last_school_city
-      t.text :student_last_school_state
+      t.string :student_grade
+      t.string :student_first_name
+      t.string :student_last_name
+      t.string :student_middle_initial
+      t.string :student_nickname
+      t.string :student_rank
+      t.string :student_total
+      t.string :student_ssn
+      t.string :student_gender
+      t.string :student_street
+      t.string :student_city
+      t.string :student_state
+      t.string :student_zip
+      t.string :student_phone
+      t.string :student_listed_in_directory
+      t.string :student_dob
+      t.string :student_dob_city
+      t.string :student_dob_state
+      t.string :student_high_school
+      t.string :student_district
+      t.string :student_race
+      t.string :student_religion
+      t.string :student_church
+      t.string :student_last_school
+      t.string :student_last_school_city
+      t.string :student_last_school_state
 
-      t.text :mother_first_name
-      t.text :mother_last_name
-      t.text :mother_email
-      t.text :mother_street
-      t.text :mother_city
-      t.text :mother_state
-      t.text :mother_zip
-      t.text :mother_alumna # text
-      t.text :mother_graduation_year
-      t.text :mother_phone_home
-      t.text :mother_phone_mobile
-      t.text :mother_phone_work
-      t.text :mother_employer
-      t.text :mother_occupation
-      t.text :mother_email_work
-      t.text :mother_marital_status
+      t.string :mother_first_name
+      t.string :mother_last_name
+      t.string :mother_email
+      t.string :mother_street
+      t.string :mother_city
+      t.string :mother_state
+      t.string :mother_zip
+      t.string :mother_alumna # string
+      t.string :mother_graduation_year
+      t.string :mother_phone_home
+      t.string :mother_phone_mobile
+      t.string :mother_phone_work
+      t.string :mother_employer
+      t.string :mother_occupation
+      t.string :mother_email_work
+      t.string :mother_marital_status
 
-      t.text :stepfather_first_name
-      t.text :stepfather_last_name
-      t.text :stepfather_email
-      t.text :stepfather_street
-      t.text :stepfather_city
-      t.text :stepfather_state
-      t.text :stepfather_zip
-      t.text :stepfather_alumnus # boolean
-      t.text :stepfather_graduation_year
-      t.text :stepfather_phone
-      t.text :stepfather_phone_mobile
-      t.text :stepfather_phone_work
-      t.text :stepfather_employer
-      t.text :stepfather_occupation
-      t.text :stepfather_email_work
+      t.string :stepfather_first_name
+      t.string :stepfather_last_name
+      t.string :stepfather_email
+      t.string :stepfather_street
+      t.string :stepfather_city
+      t.string :stepfather_state
+      t.string :stepfather_zip
+      t.string :stepfather_alumnus # boolean
+      t.string :stepfather_graduation_year
+      t.string :stepfather_phone
+      t.string :stepfather_phone_mobile
+      t.string :stepfather_phone_work
+      t.string :stepfather_employer
+      t.string :stepfather_occupation
+      t.string :stepfather_email_work
 
-      t.text :father_first_name
-      t.text :father_last_name
-      t.text :father_email
-      t.text :father_street
-      t.text :father_city
-      t.text :father_state
-      t.text :father_zip
-      t.text :father_alumnus # boolean
-      t.text :father_graduation_year
-      t.text :father_phone
-      t.text :father_phone_mobile
-      t.text :father_phone_work
-      t.text :father_employer
-      t.text :father_occupation
-      t.text :father_email_work
-      t.text :father_marital_status
+      t.string :father_first_name
+      t.string :father_last_name
+      t.string :father_email
+      t.string :father_street
+      t.string :father_city
+      t.string :father_state
+      t.string :father_zip
+      t.string :father_alumnus # boolean
+      t.string :father_graduation_year
+      t.string :father_phone
+      t.string :father_phone_mobile
+      t.string :father_phone_work
+      t.string :father_employer
+      t.string :father_occupation
+      t.string :father_email_work
+      t.string :father_marital_status
 
-      t.text :stepmother_first_name
-      t.text :stepmother_last_name
-      t.text :stepmother_email
-      t.text :stepmother_street
-      t.text :stepmother_city
-      t.text :stepmother_state
-      t.text :stepmother_zip
-      t.text :stepmother_alumna # boolean
-      t.text :stepmother_graduation_year
-      t.text :stepmother_phone
-      t.text :stepmother_phone_mobile
-      t.text :stepmother_phone_work
-      t.text :stepmother_employer
-      t.text :stepmother_occupation
-      t.text :stepmother_email_work
+      t.string :stepmother_first_name
+      t.string :stepmother_last_name
+      t.string :stepmother_email
+      t.string :stepmother_street
+      t.string :stepmother_city
+      t.string :stepmother_state
+      t.string :stepmother_zip
+      t.string :stepmother_alumna # boolean
+      t.string :stepmother_graduation_year
+      t.string :stepmother_phone
+      t.string :stepmother_phone_mobile
+      t.string :stepmother_phone_work
+      t.string :stepmother_employer
+      t.string :stepmother_occupation
+      t.string :stepmother_email_work
 
-      t.text :emergency_contact_name
-      t.text :emergency_contact_relation
-      t.text :emergency_contact_phone
+      t.string :emergency_contact_name
+      t.string :emergency_contact_relation
+      t.string :emergency_contact_phone
 
-      t.text :physician_name
-      t.text :physician_phone
-      t.text :student_allergies
-      t.text :student_medication
+      t.string :physician_name
+      t.string :physician_phone
+      t.string :student_allergies
+      t.string :student_medication
 
-      t.text :provider_first_name
-      t.text :provider_last_name
-      t.text :provider_relation
-      t.text :provider_email
-      t.text :provider_street
-      t.text :provider_city
-      t.text :provider_state
-      t.text :provider_zip
-      t.text :provider_phone
-      t.text :provider_phone_work
+      t.string :provider_first_name
+      t.string :provider_last_name
+      t.string :provider_relation
+      t.string :provider_email
+      t.string :provider_street
+      t.string :provider_city
+      t.string :provider_state
+      t.string :provider_zip
+      t.string :provider_phone
+      t.string :provider_phone_work
 
-      t.text :student_sibling_name
-      t.text :student_sibling_grade
-      t.text :student_second_sibling_name
-      t.text :student_second_sibling_grade
+      t.string :student_sibling_name
+      t.string :student_sibling_grade
+      t.string :student_second_sibling_name
+      t.string :student_second_sibling_grade
 
-      t.text :payment_option
-      t.text :legal_agreement
+      t.string :payment_option
+      t.string :legal_agreement
 
       t.timestamps
     end
