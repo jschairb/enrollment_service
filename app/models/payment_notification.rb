2e728427b83067ff594bf08fdc5529c193af367d
Josh Schairbaum <joshua.schairbaum@gmail.com>
2012-02-04 22:38:05 -0600

    it's after_save

--- app/models/payment_notification.rb
+++ app/models/payment_notification.rb
@@ -2,7 +2,7 @@ class PaymentNotification < ActiveRecord::Base
   belongs_to :enrollment
   serialize :params
 
-  after :update_enrollment_status, :on => :create
+  after_save :update_enrollment_status, :on => :create
 
   private
   def update_enrollment_status
