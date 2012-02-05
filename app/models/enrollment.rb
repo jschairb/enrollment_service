3b1a80ff398e6c09a0b88fb23b40b85fdf07a83f
Josh Schairbaum <joshua.schairbaum@gmail.com>
2012-02-04 22:22:04 -0600

    storing payment_notification in a better format

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,2 +1,3 @@
 class Enrollment < ActiveRecord::Base
+  has_many :payment_notifications
 end
--- app/models/payment_notification.rb
+++ app/models/payment_notification.rb
@@ -1,2 +1,4 @@
 class PaymentNotification < ActiveRecord::Base
+  belongs_to :enrollment
+  serialize :params
 end
